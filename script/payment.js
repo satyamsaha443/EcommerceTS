8814d7c3dc052f09bd2673120fc5d3a344a0c2be
Satyam Saha <satyamsaha443@gmail.com>
2023-10-10 01:50:47 +0530

    upd2

--- script/payment.js
+++ script/payment.js
@@ -5,11 +5,11 @@ function processPayment() {
 
     // Here, we'll just do a basic check. In real-world scenarios, you'd use a secure payment gateway.
     if (cardNumber && expiryDate && cvv) {
-        alert("Payment successful! Thank You for Shopping with us!!");
+        alert("Payment successful! (This is just a mock action.)");
         // Clear the cart after payment
         localStorage.removeItem('cart');
         window.location.href = 'index.html'; // Redirect to the main page or a confirmation page
     } else {
-        alert("Payment details invalid");
+        alert("Please fill in all the payment details.");
     }
 }
